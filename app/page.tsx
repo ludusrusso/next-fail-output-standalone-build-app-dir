ccd004373ccf35d0c51091dd5866c034306a54a5
Ludovico Russo <ludus.russo@gmail.com>
2022-12-06 18:32:44 +0100

    feat: impl

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+const IndexPage = () => {
+  return <div> index</div>;
+};
+
+export default IndexPage;
--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    appDir: true,
+  },
+  output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
